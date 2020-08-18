0ea75cc19bac6729a387fa484812320e6f9d8ddd
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2020-08-18 17:14:19 +1000

    Create ok.cc

---
+++ ok.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+class A {
+public:
+	A() :ptr(nullptr) {};
+	A(A* ptr) :ptr(ptr) {};
+	A* ptr;
+
+};
+
+int main() {
+
+	int c = 3;
+	int *d = &c;
+	
+	A *z = new A;
+	A *a = new A(d);
+	A *b = new A(a);
+	
+	delete a;
+
+  	cout << b->ptr << endl;
+	
+	return 0;
+}
